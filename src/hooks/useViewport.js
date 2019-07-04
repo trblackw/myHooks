fb0b2a7b5ebf1c2b583ff701ee3f2d35776a1acc
Tucker <trblackw@ncsu.edu>
2019-07-04 14:18:33 -0400

    Add viewport hook

---
+++ src/hooks/useViewport.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+export default () => {
+   const [windowDimensions, setWindowDimensions] = useState({ height, width });
+   const deriveWindowDimensions = () => {
+      //cross browser compatible
+      const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      setWindowDimensions({ height, width });
+   };
+
+   useEffect(
+      () => {
+         deriveWindowDimensions();
+         window.addEventListener('resize', deriveWindowDimensions);
+
+         return () => {
+            window.removeEventListener('resize', deriveWindowDimensions);
+         };
+      },
+      [height, width]
+   );
+
+   return [windowDimensions];
+};
